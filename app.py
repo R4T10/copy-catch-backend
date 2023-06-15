828eca46acc7d8864d1a7458c5c24da5e093b2f9
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-06-15 13:29:23 +0700

    change the condition

--- app.py
+++ app.py
@@ -47,45 +47,41 @@ def perform_spell_correction(text):
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files.get('file')
-    if file and file.content_type == 'application/x-zip-compressed':
-        file_stream = io.BytesIO(file.stream.read())
-
-        with zipfile.ZipFile(file_stream, 'r') as zip_file:
-            for file_name in zip_file.namelist():
-                if '/' in file_name:
-                    folder_names = file_name.split('/', 1)[0]
-                    folder_names = folder_names.replace('.', '').replace(' ', '')
-                    if folder_names.startswith('Q'):
-                        student_id = file_name.split('/')[1].split()[0]
-                        name_parts = file_name.split("-")
-                        if len(name_parts) >= 3:
-                            student_name = name_parts[2].strip()
-                        if student_id != 'Question' and 'Attempt1_textresponse' in file_name:
-                            text_response = zip_file.read(file_name).decode('utf-8')
-                            text_response = text_response.lower()
-                            if 'any text entered here will be displayed in the response input box when a new attempt ' \
-                               'at the question starts.' in text_response:
-                                text_response = text_response.replace('any text entered here will be displayed in the '
-                                                                      'response input box when a new attempt at the '
-                                                                      'question starts.', '')
-                                text_response = text_response.strip()
-                                if text_response == '':
-                                    text_response = 'null'
-                            if "'s" in text_response:
-                                text_response = text_response.replace("'s", '')
-                            if "don't" in text_response:
-                                text_response = text_response.replace("don't", 'do not')
-                            text_response = re.sub(r'[^a-zA-Z0-9 ]', ' ', text_response)
-                            text_response = perform_spell_correction(text_response)
-                            question = Question(course_id=3, question=folder_names, student_name=student_name,
-                                                student_id=student_id, answer=text_response)
-                            question_dict = question.to_dict()
-                            db.Question.insert_one(question_dict)
-                    else:
-                        return jsonify({'message': 'Invalid format'}), 400
-        return jsonify({'message': 'Upload successful'}), 200
-    else:
-        return jsonify({'message': 'Invalid type file'}), 400
+    file_stream = io.BytesIO(file.stream.read())
+    with zipfile.ZipFile(file_stream, 'r') as zip_file:
+        for file_name in zip_file.namelist():
+            if '/' in file_name:
+                folder_names = file_name.split('/', 1)[0]
+                folder_names = folder_names.replace('.', '').replace(' ', '')
+                if folder_names.startswith('Q'):
+                    student_id = file_name.split('/')[1].split()[0]
+                    name_parts = file_name.split("-")
+                    if len(name_parts) >= 3:
+                        student_name = name_parts[2].strip()
+                    if student_id != 'Question' and 'Attempt1_textresponse' in file_name:
+                        text_response = zip_file.read(file_name).decode('utf-8')
+                        text_response = text_response.lower()
+                        if 'any text entered here will be displayed in the response input box when a new attempt ' \
+                           'at the question starts.' in text_response:
+                            text_response = text_response.replace('any text entered here will be displayed in the '
+                                                                  'response input box when a new attempt at the '
+                                                                  'question starts.', '')
+                            text_response = text_response.strip()
+                            if text_response == '':
+                                text_response = 'null'
+                        if "'s" in text_response:
+                            text_response = text_response.replace("'s", '')
+                        if "don't" in text_response:
+                            text_response = text_response.replace("don't", 'do not')
+                        text_response = re.sub(r'[^a-zA-Z0-9 ]', ' ', text_response)
+                        text_response = perform_spell_correction(text_response)
+                        question = Question(course_id=3, question=folder_names, student_name=student_name,
+                                            student_id=student_id, answer=text_response)
+                        question_dict = question.to_dict()
+                        db.Question.insert_one(question_dict)
+                else:
+                    return jsonify({'message': 'Invalid format'}), 400
+    return jsonify({'message': 'Upload successful'}), 200
 
 
 @app.route('/get_data', methods=['GET'])
