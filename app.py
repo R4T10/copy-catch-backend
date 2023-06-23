2dbbacc80ca51595b427ef6a0e6c65bfe5288fb2
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-06-23 14:29:20 +0700

    setup method search  google

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 from models.question import Question
 from BM25 import BM25
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
+from googleapiclient.discovery import build
 from spellchecker import SpellChecker
 
 app = Flask(__name__)
@@ -23,6 +23,9 @@ spellchecker = SpellChecker(language='en')
 
 # Path to the coding_words.txt file
 coding_words_file = 'assets/coding_words.txt'
+
+my_api_key = "AIzaSyCkAsCqOzds-oFWnasdQlrx2ql2s2RtjWk"
+my_cse_id = "042a0393f912b4ffa"
 
 # Open the coding_words.txt file
 with open(coding_words_file, 'r') as file:
@@ -43,6 +46,12 @@ def perform_spell_correction(text):
         corrected_text.append(corrected_word)
     spell_corr = list(filter(None, corrected_text))
     return ' '.join(spell_corr)
+
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
 
 
 @app.route('/upload', methods=['POST'])
@@ -142,7 +151,7 @@ def comparingStudentAnswer():
                 df_bm = df_bm.nlargest(columns='bm25', n=3)
                 print(question)
                 print(student_id)
-                print(df_bm['student_name']+""+df_bm['answer'])
+                print(df_bm['student_name'] + "" + df_bm['answer'])
                 percentage = round((df_bm['bm25'].iloc[1] / df_bm['bm25'].iloc[0]) * 100, 2)
                 if percentage < 50:
                     percentage = 0
@@ -156,8 +165,41 @@ def comparingStudentAnswer():
         'data': list(temp.values())
     }
 
+
     return response_data, 200
-    # return 'SE'
+
+
+@app.route('/search_google', methods=['GET'])
+def searchGoogle():
+    data = db.Question.find({'course_id': 3})
+    df = pd.DataFrame(data, columns=['_id', 'course_id', 'question', 'student_id', 'student_name', 'answer'])
+    question = df['question']
+    list_q = list(set(question))
+    list_q = sorted(list_q, key=lambda x: int(x.split('-')[0][1:]))
+    temp = {}
+    for question in list_q:
+        keep = df[df['question'] == question]
+        df_question = pd.DataFrame(data=keep)
+        keep_id = keep['student_id']
+        keep_id = sorted(keep_id, key=lambda x: x[:2] + x[2:])
+        for student_id in keep_id:
+            data_for_id = df_question[df_question['student_id'] == student_id]
+            answer = data_for_id['answer'].iloc[0]
+            if answer != 'null':
+                results = google_search(answer, my_api_key, my_cse_id)
+                
+                percentage = round((df_bm['bm25'].iloc[1] / df_bm['bm25'].iloc[0]) * 100, 2)
+            else:
+                percentage = 0
+            if student_id not in temp:
+                temp[student_id] = {'student_id': student_id, 'answers': []}
+            temp[student_id]['answers'].append(percentage)
+            response_data = {
+                'question': list_q,
+                'data': list(temp.values())
+            }
+
+            return response_data, 200
 
 
 if __name__ == '__main__':
