29de0c2336a2e05b33833fad55084b62c2dae274
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-07-04 02:33:23 +0700

    adding course management route and edit some part of code

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask_cors import CORS
 from flask_pymongo import PyMongo
 import io
 import zipfile
-import json
 import re
 import pandas as pd
+from bson import ObjectId
+from models.course import Course
 from models.question import Question
 from BM25 import BM25
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from googleapiclient.discovery import build
 from spellchecker import SpellChecker
+from bson import ObjectId
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -89,6 +91,8 @@ def process_search_results(answer, snippets, vectorizer):
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files.get('file')
+    course_id = request.form['id']
+    course_id = ObjectId(course_id)
     file_stream = io.BytesIO(file.stream.read())
     with zipfile.ZipFile(file_stream, 'r') as zip_file:
         student_ids = set()
@@ -119,7 +123,7 @@ def upload():
                             text_response = text_response.replace("don't", 'do not')
                         text_response = re.sub(r'[^a-zA-Z0-9 ]', ' ', text_response)
                         text_response = perform_spell_correction(text_response)
-                        question = Question(course_id=3, question=folder_names, student_name=student_name,
+                        question = Question(course_id=course_id, question=folder_names, student_name=student_name,
                                             student_id=student_id, answer=text_response)
                         question_dict = question.to_dict()
                         db.Question.insert_one(question_dict)
@@ -134,21 +138,38 @@ def upload():
                 }
                 if db.Question.count_documents(query) == 0:
                     question_data = {
-                        'course_id': 3,
+                        'course_id': course_id,
                         'question': question,
                         'student_name': 'null',
                         'student_id': student_id,
                         'answer': 'null'
                     }
                     db.Question.insert_one(question_data)
+        db.Courses.update_one(
+            {'_id': ObjectId(course_id)},
+            {'$set': {'file': True}}
+        )
     return jsonify({'message': 'Upload successful'}), 200
 
 
 @app.route('/comparing_student', methods=['GET'])
 def comparingStudentAnswer():
     global keep_id, sorted_data_return, new_dict
-    data = db.Question.find({'course_id': 3})
+    course_id = request.args.get('id')
+    course_id = ObjectId(course_id)
+    print(course_id)
+    data = db.Question.find({'course_id': course_id})
     df = pd.DataFrame(data, columns=['_id', 'course_id', 'question', 'student_id', 'student_name', 'answer'])
+    courses = []
+    for course in data:
+        courses.append({
+            'course_id': course['course_id'],
+            'question': course['question'],
+            'student_id': course['student_id'],
+            'student_name': course['student_name'],
+            'answer': course['answer']
+        })
+    print(courses)
     question = df['question']
     list_q = list(set(question))
     list_q = sorted(list_q, key=lambda x: int(x.split('-')[0][1:]))
@@ -188,6 +209,7 @@ def comparingStudentAnswer():
                 percentage = round((df_bm['bm25'].iloc[1] / df_bm['bm25'].iloc[0]) * 100, 2)
                 if percentage < 50:
                     percentage = 0
+                print(percentage)
             else:
                 percentage = 0
             if student_id not in temp:
@@ -204,7 +226,7 @@ def comparingStudentAnswer():
 @app.route('/search_google', methods=['GET'])
 def searchGoogle():
     global results
-    data = db.Question.find({'course_id': 3})
+    data = db.Question.find({'course_id': ObjectId('64a295a45d63600866c3ced7')})
     df = pd.DataFrame(data, columns=['_id', 'course_id', 'question', 'student_id', 'student_name', 'answer'])
     question = df['question']
     list_q = list(set(question))
@@ -236,7 +258,7 @@ def searchGoogle():
                 df_bm['tfidf'] = list(scores)
                 df_bm['rank'] = df_bm['tfidf'].rank(ascending=False)
                 df_bm = df_bm.nlargest(columns='tfidf', n=5)
-                percentage = round((df_bm['tfidf'].iloc[0] * 100),2)
+                percentage = round((df_bm['tfidf'].iloc[0] * 100), 2)
                 print(answer)
                 print(df_bm[['snippet', 'tfidf', 'link']])
                 print(percentage)
@@ -258,94 +280,67 @@ def searchGoogle():
     return response_data, 200
 
 
-@app.route('/search_google2', methods=['GET'])
-def searchGoogle2():
-    answer = "ensure security groups do not allow ingress from 0 0 0 0 0 to port 3389 eco instance does not allow inbound traffic from all to tcp port 2379"
-    global keep_id, sorted_data_return, new_dict
-    data = {
-        "title": ["Ensure Security Groups do not allow ingress from 0.0.0.0/0 to port ...",
-                  "HAProxy Network Error: cannot bind socket | DigitalOcean",
-                  "Ensure Security Groups accept traffic only from ports 80 and 443"
-            , "APISIX Ingress Controller the Hard Way | Apache APISIX® -- Cloud ..",
-                  "https://opendev.org/openstack/osops/commit/c560cdc..."],
-        "snippet": [
-            "Ensure Security Groups do not allow ingress from 0.0.0.0/0 to port 3389 ... EC2 instance does not allow inbound traffic from all to TCP port 2379",
-            "Nov 4, 2020 ... The issue is that only a single process can be bound to an IP address and port combination at any given time. In the second case, when HAProxy ...",
-            "For HTTPS traffic, add an inbound rule on port 443 from the source address 0.0.0.0/0. These inbound rules allow traffic from IPv4 addresses. We recommend you ...",
-            "In this tutorial, we will install APISIX and APISIX Ingress Controller in ... ports: - name: \"client\" port: 2379 targetPort: client - name: \"peer\"",
-            "Add security rules to allow ping, ssh, docker access + 4. ... +# create a specific application layer security group that routes database port 3306 traffic, ..."],
-        "formattedUrl": ["https://docs.bridgecrew.io/docs/networking_2",
-                         "https://www.digitalocean.com/.../haproxy-network-error-cannot-bind-socket",
-                         "https://docs.bridgecrew.io/docs/networking_11",
-                         "https://apisix.apache.org/docs/ingress-controller/tutorials/the-hard-way",
-                         "https://opendev.org/.../c560cdc64dcc66a92de628bd614fcd687bca4f5c.diff"]
-    }
-    data["snippet"] = [snippet.lower() for snippet in data["snippet"]]
-    data["snippet"] = [re.sub(r'[^a-zA-Z0-9 ]', ' ', snippet) for snippet in data["snippet"]]
-    data["snippet"] = [join_text(snippet) for snippet in data["snippet"]]
-    print(data)
-    df_web = pd.DataFrame(data, columns=['title', 'snippet', 'formattedUrl'])
-    data = db.Question.find({'course_id': 3})
-    df = pd.DataFrame(data, columns=['_id', 'course_id', 'question', 'student_id', 'student_name', 'answer'])
-    question = df['question']
-    list_q = list(set(question))
-    list_q = sorted(list_q, key=lambda x: int(x.split('-')[0][1:]))
-    temp = {}
-    vectorizer = TfidfVectorizer()
-    corpus = df_web['snippet'].tolist()
-    vectorizer.fit(corpus)
-    # for question in list_q:
-    #     keep = df[df['question'] == question]
-    #     df_question = pd.DataFrame(data=keep)
-    #     keep_id = keep['student_id']
-    #     keep_id = sorted(keep_id, key=lambda x: x[:2] + x[2:])
-    #     for student_id in keep_id:
-    #         data_for_id = df_question[df_question['student_id'] == student_id]
-    #         answer = data_for_id['answer'].iloc[0]
-    #         if answer != 'null':
-    #             answer = answer.lower()
-    # test = BM25()
-    # test.fit(df_question['answer'])
-    # score = test.transform(answer)
-    # df_bm = pd.DataFrame(data=df_question)
-    # df_bm['bm25'] = list(score)
-    # df_bm['rank'] = df_bm['bm25'].rank(ascending=False)
-    # df_bm = df_bm.nlargest(columns='bm25', n=3)
-    # print('-----------------')
-    # print(df_bm['bm25'].iloc[0])
-    #
-    # answer.lower()
-    # answer = re.sub(r'[^a-zA-Z0-9 ]', ' ', answer)
-    # test = BM25()
-    # test.fit(df_web['snippet'])
-    answer_vector = vectorizer.transform([answer])
-    result_vectors = vectorizer.transform(df_web['snippet'])
-    scores = cosine_similarity(answer_vector, result_vectors)
-    scores = scores[0]
-    df_bm = pd.DataFrame(data=df_web)
-    df_bm['bm25'] = list(scores)
-    df_bm['rank'] = df_bm['bm25'].rank(ascending=False)
-    df_bm = df_bm.nlargest(columns='bm25', n=5)
+@app.route('/get_course_list', methods=['GET'])
+def get_course_list():
+    professor = 'Krit'
+    data = db.Courses.find({'professor': professor})
+    courses = []
+    for course in data:
+        courses.append({
+            'id': str(course['_id']),
+            'course_id': course['course_id'],
+            'course_name': course['course_name'],
+            'year': course['year'],
+            'examination': course['examination'],
+            'professor': course['professor'],
+            'file': course['file']
+        })
+    return jsonify(courses), 200
 
-    print(df_bm[['snippet', 'bm25', 'link']])
-    # print(df_bm['bm25'])
-    # print(df_bm['bm25'])
-    # results = google_search(answer, my_api_key, my_cse_id)
-    # scores = process_search_results(answer, results, vectorizer)
-    # max_score = max(scores)
-    # percentages = (df_bm['bm25'].iloc[0] / 100) * 100
-    # print(percentages)
-    #
-    # print(answer)
-    # print('-----------------')
-    # print(f"Search Scores: {scores}")
-    # print(f"Percentages: {percentages}")
-    # print('-----------------')
 
-    # if percentage < 50:
-    #     percentage = 0
-    # print(percentage)
-    return "Success"
+@app.route('/adding_course', methods=['POST'])
+def adding_course():
+    course_id = request.form['course_id']
+    course_name = request.form['course_name']
+    year = request.form['year']
+    examination = request.form['examination']
+    professor = request.form['professor']
+    file = False
+    course = Course(course_id=course_id, course_name=course_name, year=year,
+                    examination=examination, professor=professor, file=file)
+    question_dict = course.to_dict()
+    db.Courses.insert_one(question_dict)
+    return jsonify({'message': 'Course added successfully'}), 200
+
+
+@app.route('/delete_course', methods=['POST'])
+def delete_course():
+    course_id = request.form.get('id')
+    db.Courses.delete_one({'_id': ObjectId(course_id)})
+    db.Question.delete_many({'course_id': ObjectId(course_id)})
+    return 'Delete success', 200
+
+
+@app.route('/edit_course', methods=['POST'])
+def edit_course():
+    id = request.form.get('id')
+    id = ObjectId(id)
+    course_id = request.form['course_id']
+    course_name = request.form['course_name']
+    year = request.form['year']
+    examination = request.form['examination']
+    professor = request.form['professor']
+    db.Courses.update_one(
+        {'_id': id},
+        {'$set': {
+            'course_id': course_id,
+            'course_name': course_name,
+            'year': year,
+            'examination': examination,
+            'professor': professor
+        }}
+    )
+    return 'Edit success', 200
 
 
 if __name__ == '__main__':
