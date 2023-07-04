7afa2fcbe6830499397cc26b2660513e85e9b9c7
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-07-04 14:11:52 +0700

    adding reupload the file

--- app.py
+++ app.py
@@ -151,6 +151,71 @@ def upload():
         )
     return jsonify({'message': 'Upload successful'}), 200
 
+@app.route('/reupload', methods=['POST'])
+def reupload():
+    file = request.files.get('file')
+    course_id = request.form['id']
+    course_id = ObjectId(course_id)
+    db.Question.delete_many({'course_id': ObjectId(course_id)})
+    file_stream = io.BytesIO(file.stream.read())
+    with zipfile.ZipFile(file_stream, 'r') as zip_file:
+        student_ids = set()
+        for file_name in zip_file.namelist():
+            if '/' in file_name:
+                folder_names = file_name.split('/', 1)[0]
+                folder_names = folder_names.replace('.', '').replace(' ', '')
+                if folder_names.startswith('Q'):
+                    student_id = file_name.split('/')[1].split()[0]
+                    name_parts = file_name.split("-")
+                    if len(name_parts) >= 3:
+                        student_name = name_parts[2].strip()
+                    if student_id != 'Question' and 'Attempt1_textresponse' in file_name:
+                        student_ids.add(student_id)
+                        text_response = zip_file.read(file_name).decode('utf-8')
+                        text_response = text_response.lower()
+                        if 'any text entered here will be displayed in the response input box when a new attempt ' \
+                           'at the question starts.' in text_response:
+                            text_response = text_response.replace('any text entered here will be displayed in the '
+                                                                  'response input box when a new attempt at the '
+                                                                  'question starts.', '')
+                            text_response = text_response.strip()
+                            if text_response == '':
+                                text_response = 'null'
+                        if "'s" in text_response:
+                            text_response = text_response.replace("'s", '')
+                        if "don't" in text_response:
+                            text_response = text_response.replace("don't", 'do not')
+                        text_response = re.sub(r'[^a-zA-Z0-9 ]', ' ', text_response)
+                        text_response = perform_spell_correction(text_response)
+                        question = Question(course_id=course_id, question=folder_names, student_name=student_name,
+                                            student_id=student_id, answer=text_response)
+                        question_dict = question.to_dict()
+                        db.Question.insert_one(question_dict)
+                else:
+                    return jsonify({'message': 'Invalid format'}), 400
+        all_questions = db.Question.distinct('question')
+        for question in all_questions:
+            for student_id in student_ids:
+                query = {
+                    'question': question,
+                    'student_id': student_id
+                }
+                if db.Question.count_documents(query) == 0:
+                    question_data = {
+                        'course_id': course_id,
+                        'question': question,
+                        'student_name': 'null',
+                        'student_id': student_id,
+                        'answer': 'null'
+                    }
+                    db.Question.insert_one(question_data)
+        db.Courses.update_one(
+            {'_id': ObjectId(course_id)},
+            {'$set': {'file': True}}
+        )
+    return jsonify({'message': 'Re-upload successful'}), 200
+
+
 
 @app.route('/comparing_student', methods=['GET'])
 def comparingStudentAnswer():
